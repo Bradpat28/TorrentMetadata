6a73decdad5d73c8bd240a4fb51e3a84d03fe2ab
Bradley Patterson <bfpatter@calpoly.edu>
2018-03-20 18:04:56 -0700

    Visualization complete

--- visualize.py
+++ visualize.py
@@ -27,32 +27,32 @@ def main():
 
 
     os.chdir("data")
-    map = Basemap(projection='mill',lon_0=180)
+    map = Basemap(projection='lcc', resolution=None,
+            width=8E6, height=8E6,
+            lat_0=45, lon_0=-100,)
+    map.etopo(scale=0.5, alpha=0.5)
+    coordData = []
     for torrentData in os.listdir("."):
         if fileToLookFor == None:
             tClass = pickle.load(open(torrentData, "r" ))
             for tracker in tClass.trackerList:
                 for peer in tracker.peers:
-                    x = float(peer.loc.split(",")[0])
-                    y = float(peer.loc.split(",")[1])
-                    map.scatter(x,y)
-
-
+                    lat = float(peer.loc.split(",")[0])
+                    lon = float(peer.loc.split(",")[1])
+                    coordData.append((lon,lat))
             #tClass.printSelf()
         elif torrentData == fileToLookFor:
             tClass = pickle.load(open(torrentData, "r" ))
-    # plot coastlines, draw label meridians and parallels.
-    map.drawcoastlines()
-    map.drawparallels(np.arange(-90,90,30),labels=[1,0,0,0])
-    map.drawmeridians(np.arange(map.lonmin,map.lonmax+30,60),labels=[0,0,0,1])
-    # fill continents 'coral' (with zorder=0), color wet areas 'aqua'
-    map.drawmapboundary(fill_color='aqua')
-    map.fillcontinents(color='white',lake_color='aqua')
-    # shade the night areas, with alpha transparency so the
-    # map shows through. Use current time in UTC.
-    map.scatter(75,75)
-    plt.title('Peer ip Locations')
+
+    # plot coastlines, draw label meridians and parallels
+    # Map (long, lat) to (x, y) for plotting
+    for cord in coordData:
+        print "longitude - " + str(cord[0]) + " Latitude - " + str(cord[1])
+        x, y = map(cord[0], cord[1])
+        plt.plot(x, y, 'ok', markersize=2)
+
     plt.show()
+
 
 
 
